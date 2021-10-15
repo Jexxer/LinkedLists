ffa3dac2575d3c4dd8fad9100aa07883a839f99b
jexxer <jexxe@live.com>
2021-10-15 11:09:54 -0400

    more comments

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -9,20 +9,25 @@
 //                          .toString()
 // Play with them as you see fit.
 
+// import LinkedList
 const LinkedList = require('./LinkedList')
+
+// Create empty LinkedList
 const linkedList = new LinkedList();
 
-// Add some values to linkedList.
+// v Start playing with the methods below v
+
+// Example: Add some values to linkedList.
 linkedList.push(12);
 linkedList.push(45);
 linkedList.push(77);
 linkedList.push(28);
 
-// insert element 100 at index 3 of our LinkedList.
+// Example: insert element 100 at index 3 of our LinkedList.
 linkedList.insert(100, 3);
 
-// insert element 1 at the last index of our LinkedList.
+// Example: insert element 1 at the last index of our LinkedList.
 linkedList.insert(1, linkedList.length)
 
-// print our linkedList list in array format.
+// Example: print our linkedList list in array format.
 linkedList.print();
